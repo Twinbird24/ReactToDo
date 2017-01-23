b8d82be639bc018f762690c0fe5499187e2d5ee2
Attila Komaromi <attila.kom@gmail.com>
2017-01-23 13:18:44 -0500

    Added AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+  handleSubmit: function (e) {
+  e.preventDefault();
+
+  // take what the user inputted
+  var todoText = this.refs.todoText.value;
+
+  if (todoText.length > 0) {
+      // empty input field
+      this.refs.todoText.value = '';
+      // run the addTodo function inside of the parent component
+      this.props.onAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus();
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref="todoText" placeholder="What do you need to do?"/>
+          <button className="button expanded">Add Todo</button>
+        </form>
+      </div>
+    )
+  }
+});
+
+module.exports = AddTodo;
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -11,24 +11,4 @@ describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
   });
-
-  it('should render one Todo component for each todo item', () => {
-    var todos = [
-      {
-        id: 1,
-        text: "Do something"
-      },
-      {
-        id: 2,
-        text: "Check mail"
-      }
-    ];
-    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    // this TestUtils methods will let us check how many of a given component are rendered under a separate component
-    // i.e. check how many Todo components rendered under our TodoList component
-    var todosComponents = TestUtils.scryRenderedComponentsWithType(TodoList, Todo); // gives array of components
-
-    // check that array length above == array length of todos we made above
-    expect(todosComponents.length).toBe(todos.length);
-  });
 });
